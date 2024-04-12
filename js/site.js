0f8b901e86454d51ff9cecda7ddc53cd9820b645
Austin Denofrio <denofrioaustin@gmail.com>
2024-04-12 14:36:20 -0400

    Initial commit

---
+++ js/site.js
@@ -0,0 +1,16 @@
+// //Display the value property of input id to alert
+
+// function displayMessage() {
+//     let inputBox = document.getElementById('message');
+//     let message = inputBox.value;
+
+//     Swal.fire({
+//         title: "*APP Name*",
+//         text: message,
+//         icon: "success",
+//         backdrop: false
+    
+//       });
+      
+// }
+
